30e31d9a17dddde8f35257fbe4d8317cad953da1
Evgeniy-Bachilo <ebachilo@elinext.com>
2026-04-17 11:51:28 +0300

    Initial Commit

---
+++ TMS-study-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMS-study-project
+//
+//  Created by Evgeniy Bachilo on 17.04.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
